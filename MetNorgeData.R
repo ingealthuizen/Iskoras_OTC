dc8583e6a7abf9fee9992eaa6849636618b5718f
ingealthuizen <ingealthuizen@gmail.com>
2023-02-16 11:33:22 +0100

    use iskoras and karasjok climate stations Change dates to studyperiod
    Summarize data

--- MetNorgeData.R
+++ MetNorgeData.R
@@ -1,18 +1,18 @@
 # meteorological data Iskoras
-install.packages(jsonlite)
+#install.packages("jsonlite")
 # Libraries needed (jsonlite and tidyr must be installed in R)
 library(jsonlite)
-library(tidyr)
+library(tidyverse)
 
 # Insert your own client ID here
-client_id = '<INSERT CLIENT ID HERE>'
+client_id = "7e556d1a-8394-442c-9d7f-8d5c43cbce6e"
 
 # data is retrieved from Frost using the fromJSON function. First we build the correct URL to a Frost endpoint using parameters for sources, elements and dates.
 # Define andpoint and parameters
-endpoint <- paste0("https://", client_id, "@frost.met.no/observations/v0.jsonld")
-sources <- 'SN18700,SN90450'
-elements <- 'mean(air_temperature P1D),sum(precipitation_amount P1D),mean(wind_speed P1D)'
-referenceTime <- '2010-04-01/2010-04-03'
+endpoint <- paste0("https://", "7e556d1a-8394-442c-9d7f-8d5c43cbce6e", "@frost.met.no/observations/v0.jsonld")
+sources <- 'SN97710,SN97251'
+elements <- 'mean(air_temperature P1D),sum(precipitation_amount P1D)'
+referenceTime <- '2017-01-01/2022-12-31'
 # Build the URL to Frost
 url <- paste0(
   endpoint, "?",
@@ -22,47 +22,6 @@ url <- paste0(
 )
 # Issue an HTTP GET request and extract JSON data
 xs <- try(fromJSON(URLencode(url),flatten=T))
-
-# Check if the request worked, print out any errors
-if (class(xs) != 'try-error') {
-  df <- unnest(xs$data)
-  print("Data retrieved from frost.met.no!")
-} else {
-  print("Error: the data retrieval was not successful!")
-}
-
-
-R example script
-
-This tutorial shows you how to get started using Frost in R. The snippets of code on this page can be put together to make your own script for extracting data.
-
-First and most importantly, the client ID must be defined to be able to authenticate the requests.
-
-# Libraries needed (jsonlite and tidyr must be installed in R)
-library(jsonlite)
-library(tidyr)
-
-# Insert your own client ID here
-client_id = '<INSERT CLIENT ID HERE>'
-
-In the next block of code, data is retrieved from Frost using the fromJSON function. First we build the correct URL to a Frost endpoint using parameters for sources, elements and dates.
-
-# Define andpoint and parameters
-endpoint <- paste0("https://", client_id, "@frost.met.no/observations/v0.jsonld")
-sources <- 'SN18700,SN90450'
-elements <- 'mean(air_temperature P1D),sum(precipitation_amount P1D),mean(wind_speed P1D)'
-referenceTime <- '2010-04-01/2010-04-03'
-# Build the URL to Frost
-url <- paste0(
-  endpoint, "?",
-  "sources=", sources,
-  "&referencetime=", referenceTime,
-  "&elements=", elements
-)
-# Issue an HTTP GET request and extract JSON data
-xs <- try(fromJSON(URLencode(url),flatten=T))
-
-The above code will give an error output if something went wrong with the request. The block below will also make sure that there is valid data in the request.
 
 # Check if the request worked, print out any errors
 if (class(xs) != 'try-error') {
@@ -85,3 +44,10 @@ df2$referenceTime <- as.Date(df2$referenceTime)
 # Preview the result
 head(df2)
 
+library(lubridate)
+StudyPeriod_climate<- df2%>%
+  mutate(Year = year(referenceTime))%>%
+  group_by(sourceId, Year, elementId, unit)%>%
+  summarise(mean = mean(value),
+            sum = sum(value))
+# data does not match seklima output?
