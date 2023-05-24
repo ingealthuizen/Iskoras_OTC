a1e7469eb88ac3cf7f7c40e120dc9e3904161872
ingealthuizen <ingealthuizen@gmail.com>
2023-05-24 19:16:21 +0200

    collate 2022 flux data

---
+++ Cflux2022.R
@@ -0,0 +1,64 @@
+# combine Iskoras 2022 flux data
+library(tidyverse)
+library(lubridate)
+
+### Read in HMR output files
+CO2files_NEE <- dir(path = "C:\\Users\\ialt\\OneDrive - NORCE\\Iskoras\\Data\\Cflux\\2022\\NewMetaData", 
+             pattern = "^HMR - HMRinput_NEE.*\\CO2.csv$", full.names = TRUE, recursive = TRUE)
+
+CH4files_NEE <- dir(path = "C:\\Users\\ialt\\OneDrive - NORCE\\Iskoras\\Data\\Cflux\\2022\\NewMetaData", 
+                pattern = "^HMR - HMRinput_NEE.*\\CH4.csv$", full.names = TRUE, recursive = TRUE)
+
+# Function to read in data
+NEE_CO2data <- map_df(set_names(CO2files_NEE), function(file) {
+  file %>% 
+    set_names() %>% 
+    map_df(~ read.csv(file = file, header = TRUE, sep = ",", dec = "."))
+}, .id = "File")
+
+NEE_CO2data<- NEE_CO2data%>%
+  separate(Series, sep = "_", c("PlotID", "Treatment", "Cover", "Date", "FluxID", "H2O"))%>%
+  unite(PlotID, c(PlotID, Treatment), sep = "_", remove = FALSE)%>%
+  mutate(Date = as.Date(Date, format="%d.%m.%Y"))
+
+NEE_CH4data <- map_df(set_names(CH4files_NEE), function(file) {
+  file %>% 
+    set_names() %>% 
+    map_df(~ read.csv(file = file, header = TRUE, sep = ",", dec = "."))
+}, .id = "File")
+
+NEE_CH4data<- NEE_CH4data%>%
+  separate(Series, sep = "_", c("PlotID", "Treatment", "Cover", "Date", "FluxID", "H2O"))%>%
+  unite(PlotID, c(PlotID, Treatment), sep = "_", remove = FALSE)%>%
+  mutate(Date = as.Date(Date, format="%d.%m.%Y"))
+
+
+## SR data
+CO2files_SR <- dir(path = "C:\\Users\\ialt\\OneDrive - NORCE\\Iskoras\\Data\\Cflux\\2022\\NewMetaData", 
+                    pattern = "^HMR - HMRinput_SR.*\\CO2.csv$", full.names = TRUE, recursive = TRUE)
+
+CH4files_SR <- dir(path = "C:\\Users\\ialt\\OneDrive - NORCE\\Iskoras\\Data\\Cflux\\2022\\NewMetaData", 
+                    pattern = "^HMR - HMRinput_SR.*\\CH4.csv$", full.names = TRUE, recursive = TRUE)
+
+# Function to read in data
+SR_CO2data <- map_df(set_names(CO2files_SR), function(file) {
+  file %>% 
+    set_names() %>% 
+    map_df(~ read.csv(file = file, header = TRUE, sep = ",", dec = "."))
+}, .id = "File")
+
+SR_CO2data<- SR_CO2data%>%
+  separate(Series, sep = "_", c("PlotID", "Treatment", "Cover", "Date", "FluxID", "H2O"))%>%
+  unite(PlotID, c(PlotID, Treatment), sep = "_", remove = FALSE)%>%
+  mutate(Date = as.Date(Date, format="%d.%m.%Y"))
+
+SR_CH4data <- map_df(set_names(CH4files_SR), function(file) {
+  file %>% 
+    set_names() %>% 
+    map_df(~ read.csv(file = file, header = TRUE, sep = ",", dec = "."))
+}, .id = "File")
+
+SR_CH4data<- SR_CH4data%>%
+  separate(Series, sep = "_", c("PlotID", "Treatment", "Cover", "Date", "FluxID", "H2O"))%>%
+  unite(PlotID, c(PlotID, Treatment), sep = "_", remove = FALSE)%>%
+  mutate(Date = as.Date(Date, format="%d.%m.%Y"))
