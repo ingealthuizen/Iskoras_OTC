e276d8d7e0df2de23aaaa76e95f210f6686c13ed
ingealthuizen <ingealthuizen@gmail.com>
2023-11-09 16:29:56 +0100

    add libraries
    se calculation

--- ThawGradientAnalysis.R
+++ ThawGradientAnalysis.R
@@ -1,51 +1,55 @@
 #Thaw Gradient Analysis
+library(tidyverse)
+library(ggplot2)
+library(lubridate)
 
-#Environmental data
-# Ibutton
-###### Read in all Ibutton data 2019-2020
-Ibuttonfiles<- list.files(path = ".", 
-                          pattern =  ".*.csv",  # ELLERS BRUGE GENEREL "\\.csv$"
-                          full.names = TRUE, 
-                          recursive = TRUE)
-
-#funtion for reading Ibutton files, skipping logger info and merging temperature columns
-importIButtonFiles <- function(fileLoc) {
-  do.call(rbind, lapply(X = fileLoc, FUN = function(curFile) {
-    inText <- readLines(curFile)
-    divIndex <- which(grepl("^\\s*$", inText, perl = TRUE))[1] #skip everything until empty line
-    wholeText <- paste(sapply(X = inText[(divIndex + 1):length(inText)], FUN = function(curLine) {
-      # Reorganise the output vector so that the last entry has the correct decimalisation
-      outVec <- strsplit(curLine, ",", fixed = TRUE)[[1]]
-      outVec <- c(outVec[1:2], paste(outVec[3:length(outVec)], collapse = "."))
-      paste(outVec, collapse = ",")
-    }), collapse = "\n")
-    outOb <- read.table(text = wholeText, sep = ",", dec = ".", header = TRUE)
-    cbind(outOb, data.frame(file = rep(curFile, nrow(outOb))))
-  }))
-}
-
-setwd("C:\\Users\\ial008\\OneDrive - NORCE\\Iskoras\\Data\\2020\\Ibutton")
-IbuttonData_2020 <- importIButtonFiles(Ibuttonfiles)%>%
-  separate(file, into = c("PlotID", "Treatment", "Soildepth"), sep = "_")%>%
-  mutate(Soiltemp = Value,
-         Transect = as.factor(substring(PlotID, 3, 3)),
-         PlotID = sub("./", "", PlotID),
-         Habitat = as.factor(substring(PlotID, 2, 4)),
-         Soildepth = as.factor(sub("\\cm.csv*", "", Soildepth)),
-         DateTime = dmy_hms(Date.Time, tz = "Europe/Oslo"),
-         Time = format(DateTime, "%H:%M:%S"),
-         Date = as.Date(DateTime, format="%d/%m/%y"))%>%
-  select(-Unit, -Value, -Date.Time)%>%
-  mutate(Habitat = recode(Habitat, "WGA" = "WG"), # recode WGA and WGB to WG 
-         Habitat = recode(Habitat, "WGB" = "WG"))%>%
-  filter(Date >= '2019-06-07' & Date <= '2020-06-11')
-
-
-# Remove malfunctioning Ibuttons 
-IbuttonData_2020$Soiltemp[IbuttonData_2020$PlotID == "4S" & IbuttonData_2020$Treatment == "C" & IbuttonData_2020$Soildepth == "10"] <- NA # extreme outlier
-IbuttonData_2020$Soiltemp[IbuttonData_2020$PlotID == "4P" & IbuttonData_2020$Treatment == "C" & IbuttonData_2020$Soildepth == "10"] <- NA
-IbuttonData_2020$Soiltemp[IbuttonData_2020$PlotID == "4W" & IbuttonData_2020$Treatment == "C" & IbuttonData_2020$Soildepth == "20"] <- NA
-IbuttonData_2020$Soiltemp[IbuttonData_2020$PlotID == "3P" & IbuttonData_2020$Treatment == "OTC" & IbuttonData_2020$Soildepth == "10"] <- NA
+# function to calculate standard error
+se <- function(x) sd(x)/sqrt(length(x))
 
 #TOMST data mid june 2020- sept 2023
-TOMSTdata<-read.csv("C:\\Users\\ialt\\OneDrive - NORCE\\Iskoras\\Data\\AnalysisR\\TOMSTdata_SMcalculated2023.csv")
+TomstData<-read.csv("C:\\Users\\ialt\\OneDrive - NORCE\\Iskoras\\Data\\AnalysisR\\TOMSTdata_SMcalculated2023.csv")
+
+TomstData<-TomstData%>%
+  filter(Treatment %in% c("C", "OTC"))%>%
+  select(PlotID:LoggerID, Date, Date_Time, SoilTemperature:RawSoilmoisture, Soilmoisture_Volumetric)%>%
+  mutate(Date = as.Date(Date),
+         DateTime_UTC = as.POSIXct(strptime(Date_Time, tz="UTC", "%Y-%m-%dT%H:%M:%SZ")),
+         DateTime = format(DateTime_UTC, tz="Europe/Berlin"),
+         Hour = hour(DateTime),
+         Soilmoisture_Volumetric= Soilmoisture_Volumetric*100)
+
+# Hourly climate data per plot
+TomstData_HourlyPlotID<- TomstData%>%
+  group_by(PlotID, Transect, Habitat, Treatment, Date, Hour)%>%
+  summarise(SoilTemperature = mean(SoilTemperature, na.rm = TRUE), 
+            GroundTemperature = mean(GroundTemperature, na.rm = TRUE),
+            AirTemperature = mean(AirTemperature, na.rm = TRUE),
+            Soilmoisture_Volumetric = mean(Soilmoisture_Volumetric, na.rm = TRUE))%>%
+  ungroup()
+
+##### DAILY
+# Summary Daily Per Habitat and Treatment
+TomstData_MeanDailyHabitat<-TomstData%>%
+  gather(Climate_variable, value, SoilTemperature:Soilmoisture_Volumetric)%>%
+  group_by(Habitat, Treatment, Date, Climate_variable)%>%
+  summarise_at(vars(value), list(Min = min, Mean = mean, Max = max, sd = sd, se =se))%>%
+  mutate(Habitat =recode(Habitat, M = "Thaw slump", P= "Vegetated Palsa", S = "Bare Soil Palsa", WG= "Vegetated Pond")) # recode Habitat
+TomstData_MeanDailyHabitat$Habitat <- factor(TomstData_MeanDailyHabitat$Habitat, levels = c("Vegetated Palsa", "Bare Soil Palsa", "Thaw slump", "Vegetated Pond"))
+
+
+# main figure 2 multiple year overview and zoom in to summer?
+# missing thaw pond (W) in this figure!
+TomstData_MeanDailyHabitat%>%
+  filter(Treatment == "C")%>%
+  filter(Climate_variable %in% c("SoilTemperature"))%>%
+  #filter(Date > "2021-05-31" & Date <"2021-09-01")%>%
+  ggplot(aes(Date, Mean, col= Habitat, linetype =Treatment))+
+  geom_line()+
+  #geom_ribbon(aes(ymin = Mean-se, ymax = Mean+se, fill = Habitat), alpha=0.3) +
+  #scale_color_manual(values= c("#fc8d62", "#66c2a5", "#8da0cb"), 
+  #                   name = "Habitat")+
+  #scale_fill_manual(values= c("#fc8d62", "#66c2a5", "#8da0cb"), 
+  #                  name = "Habitat")+
+  #facet_grid(~Climate_variable, scales="free")+
+  theme_bw()+
+  theme(legend.position = "right", axis.title = element_text(size = 14), axis.text = element_text(size =12), legend.text = element_text(size =11) )
